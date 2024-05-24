5db816650b13b1c012fa24d2056e5bf7b630918c
akhsak <akhsamariyam2k2@gmail.com>
2024-05-24 21:23:43 +0530

    create authentication

---
+++ lib/controller/authontication_provider.dart
@@ -0,0 +1,166 @@
+import 'package:country_picker/country_picker.dart';
+import 'package:discover/service/authontication.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class AuthenticationProvider extends ChangeNotifier {
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  TextEditingController signInEmailController = TextEditingController();
+  TextEditingController signInPasswordController = TextEditingController();
+
+  TextEditingController fullNameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController profileEmailController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController genderController = TextEditingController();
+
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController otpController = TextEditingController();
+
+  final otpFormkey = GlobalKey<FormState>();
+  final phoneFormkey = GlobalKey<FormState>();
+  final signInFormkey = GlobalKey<FormState>();
+  final doctorAddFormKey = GlobalKey<FormState>();
+  final fillAccountFormkey = GlobalKey<FormState>();
+  final createAccountFormkey = GlobalKey<FormState>();
+  final forgotPasswordFormkey = GlobalKey<FormState>();
+
+  final AuthenticationService authenticationService = AuthenticationService();
+
+  Country selectCountry = Country(
+      phoneCode: '91',
+      countryCode: "IN",
+      e164Sc: 0,
+      geographic: true,
+      level: 1,
+      name: "INDIA",
+      example: "INDIA",
+      displayName: "INDIA",
+      displayNameNoCountryCode: "IN",
+      e164Key: "");
+
+  void notifyCountryChanged() {
+    notifyListeners();
+  }
+
+  bool showOtpField = false;
+
+  void showOtpFieldTrue() {
+    showOtpField = true;
+    notifyListeners();
+  }
+
+  String? selectedGender;
+  List<String> genders = ['Male', 'Female'];
+
+  bool signInObscureText = true;
+  void signInObscureChange() {
+    signInObscureText = !signInObscureText;
+    notifyListeners();
+  }
+
+  bool createAccountObscureText = true;
+  void createAccountObscureChange() {
+    createAccountObscureText = !createAccountObscureText;
+    notifyListeners();
+  }
+
+  bool createAccountConfirmObscureText = true;
+  void createAccountConfirmObscureChange() {
+    createAccountConfirmObscureText = !createAccountConfirmObscureText;
+    notifyListeners();
+  }
+
+  // adminKey(context, SnackBarWidget snackBarWidget, {String? message}) async {
+  //   try {
+  //     if (signInEmailController.text == 'medHeal@gmail.com' &&
+  //         signInPasswordController.text == '12345') {
+  //       Navigator.pushAndRemoveUntil(
+  //           context,
+  //           MaterialPageRoute(builder: (context) => const AdminBottomBar()),
+  //           (route) => false);
+  //       clearSignInControllers();
+  //     } else {
+  //       await emailSignIn(
+  //           signInEmailController.text, signInPasswordController.text);
+  //       Navigator.pushAndRemoveUntil(
+  //           context,
+  //           MaterialPageRoute(builder: (context) => const UserBottomBar()),
+  //           (route) => false);
+  //       clearSignInControllers();
+  //     }
+  //   } catch (error) {
+  //     snackBarWidget.showErrorSnackbar(context, message!);
+  //   }
+  // }
+
+  void deniedAdminKey() {
+    emailController.text == 'medheal';
+  }
+
+  void clearSignInControllers() {
+    signInEmailController.clear();
+    signInPasswordController.clear();
+  }
+
+  void clearCreateAccountControllers() {
+    userNameController.clear();
+    emailController.clear();
+    passwordController.clear();
+    confirmPasswordController.clear();
+  }
+
+  void clearFillProfileControllers() {
+    fullNameController.clear();
+    ageController.clear();
+    profileEmailController.clear();
+    phoneNumberController.clear();
+    genderController.clear();
+  }
+
+  void clearPhoneVerificationController() {
+    phoneController.clear();
+    otpController.clear();
+  }
+
+  Future<UserCredential> accountCreate(String email, String password) async {
+    return await authenticationService.userEmailCreate(email, password);
+  }
+
+  Future<UserCredential> emailSignIn(String email, String password) async {
+    return await authenticationService.userEmailSignIn(email, password);
+  }
+
+  Future<void> logOut() async {
+    await authenticationService.logOut();
+  }
+
+  Future<void> googleSignIn() async {
+    await authenticationService.googleSignIn();
+    notifyListeners();
+  }
+
+  Future<void> googleSignOut() async {
+    await authenticationService.googleSignOut();
+    notifyListeners();
+  }
+
+  Future<void> getOtp(phoneNumber) async {
+    await authenticationService.getOtp(phoneNumber);
+    notifyListeners();
+  }
+
+  Future<void> verifyOtp(otp, context, {error}) async {
+    await authenticationService.verifyOtp(otp, context, error);
+    notifyListeners();
+  }
+
+  Future<void> forgotPassword(context, {email}) async {
+    authenticationService.passwordReset(email: email, context: context);
+  }
+}
---
+++ lib/service/authontication.dart
@@ -0,0 +1,152 @@
+import 'dart:developer';
+import 'package:discover/widgets/bottombar.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthenticationService {
+  String collection = 'user';
+  String? verificationid;
+
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  FirebaseFirestore fireStore = FirebaseFirestore.instance;
+
+  Future<UserCredential> userEmailCreate(String email, String password) async {
+    try {
+      UserCredential userCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      log('Account created');
+
+      return userCredential;
+    } catch (error) {
+      log('error got in creating account $error ');
+      rethrow;
+    }
+  }
+
+  Future<UserCredential> userEmailSignIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+      log('Signed In');
+      return userCredential;
+    } on FirebaseAuthMultiFactorException catch (error) {
+      throw Exception(error.code);
+    }
+  }
+
+  Future<void> logOut() async {
+    await firebaseAuth.signOut();
+  }
+
+  Future<void> googleSignIn() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      if (googleAuth == null) {
+        log('Google authentication failed');
+        throw Exception('Google authentication failed');
+      }
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final User? guser = userCredential.user;
+      log("${guser?.displayName}");
+    } catch (error) {
+      log('Google SignIn error : $error');
+      rethrow;
+    }
+  }
+
+  Future googleSignOut() async {
+    return await GoogleSignIn().signOut();
+  }
+
+  Future<void> getOtp(String phoneNumber) async {
+    try {
+      await firebaseAuth.verifyPhoneNumber(
+        phoneNumber: phoneNumber,
+        verificationCompleted: (phoneAuthCredential) async {
+          await firebaseAuth.signInWithCredential(phoneAuthCredential);
+          User? user = FirebaseAuth.instance.currentUser;
+          if (user != null) {
+            await user.updatePhoneNumber(phoneAuthCredential);
+          }
+        },
+        verificationFailed: (error) {
+          log("verification failed error : $error");
+        },
+        codeSent: (verificationId, forceResendingToken) {
+          verificationid = verificationId;
+        },
+        codeAutoRetrievalTimeout: (verificationId) {
+          verificationid = verificationId;
+        },
+        timeout: const Duration(seconds: 60),
+      );
+    } catch (e) {
+      log("sign in error : $e");
+    }
+  }
+
+  Future<PhoneAuthCredential?> verifyOtp(
+      otp, context, Function? snackBarError) async {
+    try {
+      log('message');
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+          verificationId: verificationid!, smsCode: otp);
+      await firebaseAuth.signInWithCredential(credential);
+      log('message');
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>BottomScreen(),
+          ),
+          (route) => false);
+    } catch (e) {
+      snackBarError;
+      log("verify otp error $e");
+      return null;
+    }
+    return null;
+  }
+
+  void passwordReset({required email, context}) async {
+    try {
+      log('start');
+      await firebaseAuth.sendPasswordResetEmail(email: email);
+      log('success');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Password reset email sent"),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      log('error occure');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.message.toString(),
+          ),
+        ),
+      );
+    }
+  }
+
+  // updateUser(userid, UserModel data) async {
+  //   try {
+  //     await user.doc(userid).update(data.toJson());
+  //   } catch (e) {
+  //     log("error in updating product : $e");
+  //   }
+  // }
+}
